484fd66871acb7a7a85a4a77aec847699ca8f9bc
Daniel Moreira <danielmoreira92@gmail.com>
2019-12-03 09:08:48 +0000

    Updates

---
+++ precache-manifest.0250688ea16b5358feee67e783b71b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220167caa765684709b1a8119a35b1df",
+    "url": "/carpooling/index.html"
+  },
+  {
+    "revision": "753bb75785eae7736baf",
+    "url": "/carpooling/static/css/2.3e5da724.chunk.css"
+  },
+  {
+    "revision": "2a83baf0c46531c11559",
+    "url": "/carpooling/static/css/main.6613f821.chunk.css"
+  },
+  {
+    "revision": "753bb75785eae7736baf",
+    "url": "/carpooling/static/js/2.0dd57ce2.chunk.js"
+  },
+  {
+    "revision": "2a83baf0c46531c11559",
+    "url": "/carpooling/static/js/main.c04ed254.chunk.js"
+  },
+  {
+    "revision": "63b95a7f407759aa3025",
+    "url": "/carpooling/static/js/runtime-main.7ccb42e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carpooling/precache-manifest.445860e404205e4a4c38fd0159d5a197.js"
+  "/carpooling/precache-manifest.0250688ea16b5358feee67e783b71b9b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/carpooling/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carpooling/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
